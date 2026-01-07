0f7bab57a2b40003b62f192b2164b9f14d0065b7
Jessica Taylor <rstudio@Jessica-Laptop.localdomain>
2026-01-07 15:03:51 +0000

    Fuzzy k-means optimised for clustering 60% DEGs.

--- DGE analysis.R
+++ DGE analysis.R
@@ -2,6 +2,8 @@ library(stringr)
 library(DESeq2)
 library(hash)
 library(ggplot2)
+library(pheatmap)
+library(paletteer)
 
 # Import raw read counts.
 rawCounts <- data.frame(Gene = read.table("Counts_data/Col-0/Col_F0_1_readCounts.txt")[,1])
@@ -50,3 +52,74 @@ for (time in c("F30", "F90", "F180")) {
 
 DEGs <- unique(DEGs)
 DEGs_normCounts <- normDDS[which(rownames(normDDS) %in% DEGs),]
+
+# Calculate Z-scores
+source("Functions/Calculate_Zscores.R")
+DEGs_normCounts <- calculate_Zscore(DEGs_normCounts)
+
+# Fuzzy k-means analysis.
+Zscores <- as.matrix(DEGs_normCounts[,which(str_detect(colnames(DEGs_normCounts), "Zscore")==TRUE)])
+
+set.seed(20)
+
+fuzzyClusteringData <- ExpressionSet(assayData = as.matrix(Zscores))
+m <- mestimate(fuzzyClusteringData)
+fuzzClust <- mfuzz(fuzzyClusteringData, centers = 6, m = m)
+
+# Remove genes that cannot be assigned to a cluster with a membership value > 0.7.
+goodGenes <- acore(fuzzyClusteringData, cl=fuzzClust, min.acore = .5)
+genesToCluster <- c()
+for (c in 1:length(goodGenes)) {
+  genesToCluster <- append(genesToCluster, which(names(fuzzClust$cluster) %in% goodGenes[[c]]$NAME))
+}
+
+fuzzClust$cluster <- fuzzClust$cluster[genesToCluster]
+fuzzyClusteringData <- ExpressionSet(assayData = fuzzyClusteringData@assayData$exprs[which(rownames(fuzzyClusteringData@assayData$exprs) 
+                                                                                           %in% names(fuzzClust$cluster)),])
+# Repeat clustering on high-confidence genes.
+m <- mestimate(fuzzyClusteringData)
+fuzzClust <- mfuzz(fuzzyClusteringData, centers = 6, m = m)
+
+fuzzyClusteringData <- as.data.frame(fuzzyClusteringData@assayData$exprs)
+fuzzyClusteringData$cluster <- as.data.frame(fuzzClust$cluster)[,1]
+
+# Plot a boxplot of Z-scores at each time point in each cluster.
+clusterCentres <- data.frame()
+for (clust in 1:6) {
+  isolateCluster <- fuzzyClusteringData[fuzzyClusteringData$cluster==clust,]
+  
+  coreGenes <- c()
+  for (col in 1:(ncol(isolateCluster)-1)) {
+    coreGenes <- append(coreGenes, rownames(isolateCluster[which(isolateCluster[,col] >= quantile(isolateCluster[,col], probs = .25) &
+                                                                   isolateCluster[,col] <= quantile(isolateCluster[,col], probs = .75)),]))
+  }
+  coreGenes <- unique(coreGenes)
+  
+  isolateCluster <- isolateCluster[which(rownames(isolateCluster) %in% coreGenes),]
+  
+  df <- data.frame()
+  
+  for (col in 1:(ncol(isolateCluster)-1)) {
+    df <- rbind(df, data.frame(Cluster = rep(clust, times = nrow(isolateCluster)),
+                               Time = rep(colnames(isolateCluster)[col], times = nrow(isolateCluster)),
+                               Zscore = isolateCluster[,col]))
+  } 
+  clusterCentres <- rbind(clusterCentres, df)
+}
+clusterCentres$Time <- str_match(clusterCentres$Time, "^[a-zA-Z]+([0-9]+)_Zscore")[,2]
+clusterCentres$Cluster <- paste("Cluster", clusterCentres$Cluster)
+
+clusterBoxplot <- ggplot(clusterCentres, aes(x = factor(Time, levels = c("0", "5", "10", "30", "90", "180")), y = Zscore)) + 
+  geom_boxplot() + xlab("Time (min)") + ylab("Z-score") + facet_wrap(~Cluster) + theme_bw() 
+
+fuzzyClusteringData$cluster <- factor(fuzzyClusteringData$cluster, 
+                                         levels = c(4,5,3,6,2,1))
+
+pheatmap(fuzzyClusteringData[order(fuzzyClusteringData$cluster),c(1:4)],
+         color = rev(paletteer_d("colorBlindness::ModifiedSpectralScheme11Steps")),
+         cluster_rows = FALSE,
+         cluster_cols = FALSE,
+         show_rownames = FALSE,
+         annotation_names_row = FALSE,
+         annotation_legend = FALSE,
+         scale = "row", fontsize = 16)
---
+++ Functions/Calculate_Zscores.R
@@ -0,0 +1,27 @@
+calculate_Zscore <- function(normCounts) {
+  
+  # Calculate the average counts per time point.
+  avgColNames <- c()
+  for (time in c("F0","F30","F90","F180")) {
+    normCounts <- cbind(normCounts, rowMeans(as.matrix(normCounts[,which(str_detect(colnames(normCounts), time)==TRUE)])))
+  
+    avgColNames <- append(avgColNames, paste0(time, "_avg"))
+  }
+  colnames(normCounts) <- c(colnames(normCounts)[1:(ncol(normCounts)-length(avgColNames))], avgColNames)
+ 
+  # Calculate mean and sd across conditions.
+  normCounts <- cbind(normCounts, rowMeans(as.matrix(normCounts[,avgColNames])))
+  normCounts <- cbind(normCounts, rowSds(as.matrix(normCounts[,avgColNames])))
+  
+  colnames(normCounts) <- c(colnames(normCounts)[1:(ncol(normCounts)-2)], "pop_avg", "pop_sd")
+  
+  # Calculate Z-scores.
+  ZcolNames <- c()
+  for (col in which(str_detect(colnames(normCounts), "[A-Za-z]+[0-9]+_avg") == TRUE)) {
+    normCounts <- cbind(normCounts, (normCounts[,col]-normCounts[,"pop_avg"])/normCounts[,"pop_sd"])
+    
+    ZcolNames <- append(ZcolNames, paste0(str_match(colnames(normCounts)[col], "^(.*)avg")[,2],"Zscore"))
+  }
+  colnames(normCounts) <- c(colnames(normCounts)[1:(ncol(normCounts)-4)], ZcolNames)
+  return(normCounts) 
+}
