a919943a8dc504e944e9ed82e23dd70579f9a4e3
Jessica Taylor <rstudio@Jessica-Laptop.localdomain>
2026-01-07 12:37:51 +0000

    Modify code for analysing raw RNA-seq data, with 0 min time-point as reference.

--- DGE analysis.R
+++ DGE analysis.R
@@ -1,83 +1,52 @@
-setwd("~/nextflow-peakcaller/Differential-expression-analysis")
-
-# Load required libraries.
-source("Functions for DGE analysis.R")
-loadLibraries()
-rm(loadLibraries)
+library(stringr)
+library(DESeq2)
+library(hash)
+library(ggplot2)
 
 # Import raw read counts.
-allCounts <- read.csv("Data/PRJEB25079.csv")
-genes <- allCounts[,1]
-allCounts <- allCounts[,-1]
-row.names(allCounts) <- genes
-
-rm(genes)
-
-# Generate a hash mimicking an annotation file that will define the comparisons across conditions.
-colData <- hash()
-
-for (treatment in unique(str_match(colnames(allCounts), "^([a-zA-Z]+).*$")[,2])[-1]) {
-  colData[[treatment]] <- hash(`0min` = data.frame(sample = colnames(allCounts)[which(grepl("mock0", colnames(allCounts))==TRUE | grepl(paste0(treatment, "0"), colnames(allCounts))==TRUE)],
-                                                condition = str_match(colnames(allCounts)[which(grepl("mock0", colnames(allCounts))==TRUE | grepl(paste0(treatment, "0"), colnames(allCounts))==TRUE)], "^([A-Za-z]+0)_[0-9]+$")[,2]),
-                            `5min` = data.frame(sample = colnames(allCounts)[which(grepl("mock5", colnames(allCounts))==TRUE | grepl(paste0(treatment, "5"), colnames(allCounts))==TRUE)],
-                                                condition = str_match(colnames(allCounts)[which(grepl("mock5", colnames(allCounts))==TRUE | grepl(paste0(treatment, "5"), colnames(allCounts))==TRUE)], "^([A-Za-z]+5)_[0-9]+$")[,2]),
-                            `10min` = data.frame(sample = colnames(allCounts)[which(grepl("mock10", colnames(allCounts))==TRUE | grepl(paste0(treatment, "10"), colnames(allCounts))==TRUE)],
-                                                 condition = str_match(colnames(allCounts)[which(grepl("mock10", colnames(allCounts))==TRUE | grepl(paste0(treatment, "10"), colnames(allCounts))==TRUE)], "^([A-Za-z]+10)_[0-9]+$")[,2]),
-                            `30min` = data.frame(sample = colnames(allCounts)[which(grepl("mock30", colnames(allCounts))==TRUE | grepl(paste0(treatment, "30"), colnames(allCounts))==TRUE)],
-                                                 condition = str_match(colnames(allCounts)[which(grepl("mock30", colnames(allCounts))==TRUE | grepl(paste0(treatment, "30"), colnames(allCounts))==TRUE)], "^([A-Za-z]+30)_[0-9]+$")[,2]),
-                            `90min` = data.frame(sample = colnames(allCounts)[which(grepl("mock90", colnames(allCounts))==TRUE | grepl(paste0(treatment, "90"), colnames(allCounts))==TRUE)],
-                                                 condition = str_match(colnames(allCounts)[which(grepl("mock90", colnames(allCounts))==TRUE | grepl(paste0(treatment, "90"), colnames(allCounts))==TRUE)], "^([A-Za-z]+90)_[0-9]+$")[,2]),
-                            `180min` = data.frame(sample = colnames(allCounts)[which(grepl("mock180", colnames(allCounts))==TRUE | grepl(paste0(treatment, "180"), colnames(allCounts))==TRUE)],
-                                                  condition = str_match(colnames(allCounts)[which(grepl("mock180", colnames(allCounts))==TRUE | grepl(paste0(treatment, "180"), colnames(allCounts))==TRUE)], "^([A-Za-z]+180)_[0-9]+$")[,2]))
+rawCounts <- data.frame(Gene = read.table("Counts_data/Col-0/Col_F0_1_readCounts.txt")[,1])
+for (file in list.files("Counts_data/Col-0")) {
+  rawCounts <- cbind(rawCounts, read.table(paste0("Counts_data/Col-0/",file))[,2])
 }
 
+# Set row names to gene IDs
+rownames(rawCounts) <- rawCounts$Gene
+rawCounts <- rawCounts[,-1]
 
-# Create workbook into which the output data will be saved.
-DGE_output <- createWorkbook()
-DGE_output_filtered <- createWorkbook()
+# Set column names to sample IDs
+colnames(rawCounts) <- str_match(list.files("Counts_data/Col-0"), "^(.*)_readCounts.txt")[,2]
 
-times = c("0min", "5min", "10min", "30min", "90min", "180min")
+# Create metadata file.
+colData <- data.frame(Sample = as.factor(colnames(rawCounts)),
+                      Time = as.factor(str_match(colnames(rawCounts),"^Col_(F[0-9]+)_[0-9]+$")[,2]))
 
-for (treatment in names(colData)) {
-  addWorksheet(DGE_output, sheetName = treatment)
-  addWorksheet(DGE_output_filtered, sheetName = paste(treatment, "normCounts", sep = "_"))
-  addWorksheet(DGE_output_filtered, sheetName = paste(treatment, "logFC", sep = "_"))
+rownames(colData) <- colnames(rawCounts)
+
+# Construct DESeq dataset.
+dds <- DESeqDataSetFromMatrix(countData=rawCounts, colData=colData, design=~Time)
+dds$Time <- relevel(dds$Time, ref = "F0")
+
+# Remove genes with < 10 reads.
+dds <- dds[rowSums(counts(dds)) >= 10,]
+
+# Run the DESeq.
+DDS <- DESeq(dds)
+normDDS <- counts(DDS, normalized = TRUE) # normalization with respect to the sequencing depth
+write.csv(normDDS, "Results/Col-0/Normalised_counts.csv")
+
+# Save the results.
+DEGs <- c()
+
+for (time in c("F30", "F90", "F180")) {
+  res <- results(DDS, contrast = c("Time", time, "F0"), independentFiltering = FALSE)
+  res <- res[,c(2,6)]
+  colnames(res) <- c(paste0(time, "_FC"), paste0(time, "_padj"))
   
-  # Construct DESeq dataset.
-  comparison <- data.frame()
-  for (time in times) {
-    comparison <- rbind(comparison, data.frame(samples = colData[[treatment]][[time]]$sample,
-                                               conditions = colData[[treatment]][[time]]$condition))
-  }
-  counts <- allCounts[,comparison$samples]
-  dds <- DESeqDataSetFromMatrix(countData=counts, colData=comparison, design=~conditions)
-  rm(comparison)
+  write.csv(res, paste0("Results/Col-0/", time, "_vs_F0.csv"))
   
-  # Remove genes with < 10 reads.
-  dds <- dds[rowSums(counts(dds)) >= 10,]
-  
-  # Run the DESeq pipeline.
-  normalisedCounts <- run_DESeq(dds, treatment)
-  rm(dds, counts)
-  
-  allNormalisedCounts <- data.frame(Genes = rownames(normalisedCounts))
-  for (time in str_match(times, "^([0-9]+).*$")[,2]) {
-    allNormalisedCounts <- cbind(allNormalisedCounts, data.frame(rowMeans(normalisedCounts[,which(!is.na(str_extract(colnames(normalisedCounts), paste0("^(",treatment,time,"_[1-9]+).*$"))))])))
-    
-  }
-  colnames(allNormalisedCounts) <- c("Genes",paste0(treatment, str_match(times, "^([0-9]+).*$")[,2],"_avg"))
-  writeData(DGE_output, sheet = treatment, allNormalisedCounts)
-  
-  # Filter for genes with logFC < -1 or > 1 and padj <= .01 in at least one time point
-  normalisedCounts <- filterDEGs(normalisedCounts, 1, .01, DGE_output_filtered, treatment)
-  
-  # Calculate Z-scores.
-  normalisedCounts <- calculate_Zscores(normalisedCounts, treatment)
-  
-  # Save output.
-  normalisedCounts <- cbind(Gene = rownames(normalisedCounts), normalisedCounts)
-  writeData(DGE_output_filtered, sheet = paste(treatment, "normCounts", sep = "_"), normalisedCounts)
+  # Get list of DEGs with logFC < -1 or > 1 and padj <= .01
+  DEGs <- append(DEGs, rownames(res[which(res[,1]>=1 | res[,1]<=-1 & res[,2]<=.01),]))
 }
-saveWorkbook(DGE_output, "DGE analysis output.xlsx", overwrite = TRUE) 
-saveWorkbook(DGE_output_filtered, "DGE analysis output (filtered).xlsx", overwrite = TRUE) 
-rm(run_DESeq, filterDEGs, calculate_Zscores)
+
+DEGs <- unique(DEGs)
+DEGs_normCounts <- normDDS[which(rownames(normDDS) %in% DEGs),]
